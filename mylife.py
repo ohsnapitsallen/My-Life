78ea5b034fefc6f8cd8c829f93eb60889c775e7e
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:54:57 +0800

    Pseudocodes

---
+++ mylife.py
@@ -0,0 +1,6 @@
+#Create a function where the program asks the user to enter lines then writes the input to the text fil
+#Start the function
+#Create a loop to ask the user if they want to write more lines to the file
+#Starts the function again if user wants to write more
+#Ends the program if the user is done
+#Restarts the loop if the input is an invalid response
