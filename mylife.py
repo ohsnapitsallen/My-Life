d6f15282048560a14507630e1acab478440f733f
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 20:01:17 +0800

    While loop added with start function

--- mylife.py
+++ mylife.py
@@ -6,6 +6,10 @@ def mylife():
 
 morelines = ' '
 #Create a loop to ask the user if they want to write more lines to the file
+while morelines != "n":
+    mylife()
+    print("Are there more lines? y/n")
+    morelines = input()
 #Starts the function again if user wants to write more
 #Ends the program if the user is done
 #Restarts the loop if the input is an invalid response
