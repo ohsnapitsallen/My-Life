69f0553527a29f2181c4cc94ded5b56b216a92c5
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:57:45 +0800

    Pseudocodes fixed

--- mylife.py
+++ mylife.py
@@ -1,5 +1,4 @@
-#Create a function where the program asks the user to enter lines then writes the input to the text fil
-#Start the function
+#Create a function where the program asks the user to enter lines then writes the input to the text file
 #Create a loop to ask the user if they want to write more lines to the file
 #Starts the function again if user wants to write more
 #Ends the program if the user is done
