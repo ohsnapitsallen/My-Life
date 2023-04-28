80b67e7444b7696c7b4e682c76bade5dbf0345fe
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 20:08:59 +0800

    Error Fixed: function repeats in loop instead of y/n question when input is invalid

--- mylife.py
+++ mylife.py
@@ -5,17 +5,18 @@ def mylife():
         output_file.write(str(content) + "\n")
 
 morelines = ' '
+#Start the function
+mylife()
 #Create a loop to ask the user if they want to write more lines to the file
 while morelines != "n":
-    mylife()
-    print("Are there more lines? y/n")
-    morelines = input()
+  print("Are there more lines? y/n")
+  morelines = input()
 #Starts the function again if user wants to write more
   if morelines == "y":
-      print("Please enter another line")
-      #Ends the program if the user is done
+    mylife()
+    #Ends the program if the user is done
   elif morelines == "n":
-      break
-#Restarts the loop if the input is an invalid response
-   else:
-       print("Invalid input, please try again.")
+     break
+    #Restarts the loop if the input is an invalid response
+  else:
+     print("Invalid input, please try again.")
